08bbbb9aa9b298b3693c16a0b57031382cf227dd
CarlosMurta <carlosmurtapombo@gmail.com>
2018-06-12 19:14:28 -0300

    Atualização versão Carlos do código Números Primos

--- simpleController/src/main/java/com/example/simpleController/services/PrimeNumberService.java
+++ simpleController/src/main/java/com/example/simpleController/services/PrimeNumberService.java
@@ -1,21 +1,50 @@
-package com.example.simpleController.services;
+//package com.example.simpleController.services;
 
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@Service
+//@Service
 public class PrimeNumberService {
 
-    public List<Integer> findPrimeNumbersUntil(int endNumber){
-        //fim
+//    public List<Integer> findPrimeNumbersUntil(int endNumber){
 
-        List<Integer> primeNumberList = new ArrayList<>();
+//        List<Integer> primeNumberList = new ArrayList<>();
 
-        //preencher lista
+    public static void main(String[] args){
 
-        return Arrays.asList(1,2,3);
+        int endNumber = 10;
+        int arrayPrime [] = new int[endNumber];
+        int i = 1; // Índice da Array
+        int x = 3; // Primeiro número a ser testado, até o limite endNumbers.
+        int n = 2; // Primeiro número da divisão para testar o resto.
+
+        arrayPrime[0] = 2;
+
+        while (x < endNumber){
+
+            n = 2;
+
+            while (n < (x - 1)){
+
+                if ((x % n) != 0){ break; }
+
+                n++;
+
+            }
+
+            if (n == x){ arrayPrime[i] = x; i++; }
+
+            x++;
+
+        }
+
+        System.out.println(Arrays.toString(arrayPrime));
+
+
+
+        //return Arrays.asList(1,2,3);
+
     }
 }
